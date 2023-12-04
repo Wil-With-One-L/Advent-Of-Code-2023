675c0d81c0e3852ba26b3b18483d99099f1c62c4
Wil-With-One-L <99309050+Wil-With-One-L@users.noreply.github.com>
2023-12-04 13:21:24 -0600

    Day 4 Part 2 Complete

--- Day 4/Day4.py
+++ Day 4/Day4.py
@@ -12,20 +12,53 @@ def cleanInput(filename):
     lines[i][1] = lines[i][1].split(" ")
   return lines
 
+# maps card idx to number of numbers that match winning numbers
+def getMatchingNums(lines):
+  output = []
+  for card in lines:
+    matching = 0
+    for winning in card[0]:
+      if winning in card[1]:
+        matching += 1
+      
+    output.append(matching)
+
+  return output
+    
+# Returns a dict with 0-219 mapped to 1
+def makeCopies():
+  ret = dict()
+  for i in range(0, 220):
+    ret[i] = 1
+  return ret
+
+def getTotal(copies):
+  total = 0
+  for i in range(0, 220):
+    total += copies[i]
+  return total
+
 lines = cleanInput("Day 4/input.txt")
+matching_nums = getMatchingNums(lines)
+copies = makeCopies()
+print(matching_nums)
 
-points = 0
-for l in lines:
-  matching = 0
-  for winning in l[0]:
-    if winning in l[1]:
-      matching += 1
-  if matching > 0:
-    points += (2 ** (matching - 1))
+# add a copy of the next (matching_nums[i]) cards
 
-print(points)
+for idx in range(0, 220):
+  matches = matching_nums[idx]
+
+  if matches > 0:
+    for i in range(idx + 1, idx + matches + 1):
+      copies[i] = copies[i] + (copies[idx])
+
+total = getTotal(copies)
+print(total)
+
 
 # 960 too low
 # 52728 too high
 # 52508 too high
 # Part 1 - 26346
+
+# Part 2 - 8467762
